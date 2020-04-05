dc72aca571cc838067dc2ff68bc02606ba233ae9
Unknown <218435@student.pwr.edu.pl>
2020-04-05 22:19:26 +0200

    JWT Authorization
    -JWT Authorization
    - bug fix

--- volunteerAngularApp/src/app/_services/user.service.ts
+++ volunteerAngularApp/src/app/_services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '@/_models/user';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({ providedIn: 'root' })
 export class UserService {
+    constructor(private http: HttpClient) { }
 
-  constructor() { }
+    getAll() {
+        return this.http.get<User[]>(`localhost:8080/users`);
+    }
+
+    getById(id: number) {
+        return this.http.get<User>(`localhost:8080/users/${id}`);
+    }
 }
