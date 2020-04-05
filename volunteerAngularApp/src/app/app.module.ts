becd6f4419dd69f7bb85660ed47256c2021a6a51
Unknown <218435@student.pwr.edu.pl>
2020-04-05 21:08:15 +0200

    Add Role Based Authorization
    - authorization
    - navigation bar
    - roles

--- volunteerAngularApp/src/app/app.module.ts
+++ volunteerAngularApp/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { routing } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { JwtInterceptor, ErrorInterceptor, fakeBackendProvider } from './_helpers'; // used to create fake backend
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [
+      { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+      { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+      // provider used to create fake backend
+      fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
