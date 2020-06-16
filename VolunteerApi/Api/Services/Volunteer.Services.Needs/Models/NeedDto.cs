73980c551fffa05be418c2eee67753925850b19a
Konrad <slota.k@outlook.com>
2020-06-16 22:35:07 +0200

    Need service created

---
+++ VolunteerApi/Api/Services/Volunteer.Services.Needs/Models/NeedDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Volunteer.Services.Needs.Models
+{
+    public class NeedDto
+    {
+        public int NeedId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DeadlineDate { get; set; }
+    }
+}
---
+++ VolunteerApi/Api/Services/Volunteer.Services.Needs/Services/NeedService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volunteer.DAL;
+using Volunteer.Services.Needs.Models;
+
+namespace Volunteer.Services.Needs.Services
+{
+    public class NeedService
+    {
+        private readonly DalContext _dalContext;
+
+        public ICollection<NeedDto> GetNeeds()
+        {
+            return new List<NeedDto>();
+        }
+    }
+}
